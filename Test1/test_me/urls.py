7c490d6a8fa211e19747978978b10c59e2c0566f
unknown <Mewkkn@hotmail.com>
2019-02-11 20:43:33 +0700

    Input number multiplication

--- Test1/test_me/urls.py
+++ Test1/test_me/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+app_name = 'test_me'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:id>/', views.multi, name='multi'),
+    #path('<int:id>/', views.multi, name='multi'), #3number
+    #path('<int:id>/', views.multiplication, name='multipi'), #multiplication
+    path('', views.inputnum, name='inputnum'),
+    path('multipli/', views.multiplication, name='multipli'),
 ]
--- Test1/test_me/views.py
+++ Test1/test_me/views.py
@@ -1,12 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("Hello")
-
 def multi(request,id):
     num = str(id) *3
     context = {
         'number': num
     }
     return render(request,'index.html',context)
+
+def multiplication(request):
+    a = []
+    for i in range(1,13):
+        answer = int(request.POST['number']) * i
+        a.append(answer)
+    print(a)
+    context = {
+        'id': request.POST['number'],
+        'num': a
+    }
+    return render(request, 'multi.html', context)
+
+def inputnum(request):
+    return render(request,'index.html')
